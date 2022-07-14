7482c647affad182a19a30b37a499cf1c451e3f4
sina <sinaolfati5@gmail.com>
2022-07-14 17:02:15 +0430

    adding touched as a condition to show the errors

--- src/components/Input.js
+++ src/components/Input.js
@@ -41,19 +41,19 @@ console.log(formik.touched)
         <div className='controler'>
         <label className="label" htmlFor = "name">Name</label>
         <input type="text" id = "name" name = "name" onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}/>
-        {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+        {formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
         </div>
 
         <div className='controler'>
         <label htmlFor = "email">Email</label>
         <input type="email" id = "email" name = "email" onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur}/>
-        {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+        {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
         </div>
 
         <div className='controler'>
         <label htmlFor = "channel">Channel</label>
         <input type="text" id = "channel" name = "channel" onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur}/>
-        {formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+        {formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
         </div>
 
         <button type="submit">Submit</button>
