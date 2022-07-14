1e4896e7b96666b47976a0dc5137498aad327f47
sina <sinaolfati5@gmail.com>
2022-07-14 15:33:07 +0430

    using initialValues and passing vlaues from form to useFormik hook

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,20 +3,28 @@ import "./Input.css"
 import { useFormik } from 'formik'
 
 const Input = () => {
-const formik = useFormik({})
+const formik = useFormik({
+    initialValues: {
+        name: "",
+        email: "",
+        channel: "",
+    }
+})
+
+console.log(formik.values)
 
 
   return (
     <div className='container'>
       <form>
         <label className="label" htmlFor = "name">Name</label>
-        <input type="text" id = "name" name = "name"/>
+        <input type="text" id = "name" name = "name" onChange={formik.handleChange} value={formik.values.name}/>
         
         <label htmlFor = "email">Email</label>
-        <input type="email" id = "email" name = "email"/>
+        <input type="email" id = "email" name = "email" onChange={formik.handleChange} value={formik.values.email}/>
         
         <label htmlFor = "channel">Channel</label>
-        <input type="text" id = "channel" name = "channel"/>
+        <input type="text" id = "channel" name = "channel" onChange={formik.handleChange} value={formik.values.channel}/>
 
         <button>Submit</button>
       </form>
