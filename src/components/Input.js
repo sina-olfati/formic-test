dc31d1076b66bda314352df78e3cb0f39882429e
sina <sinaolfati5@gmail.com>
2022-07-15 18:06:49 +0430

    Schema validation with Yup

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Input.css"
-import { useFormik } from 'formik'
+import { useFormik, yupToFormErrors } from 'formik'
+import * as Yup from "yup"
 
 const Input = () => {
 const formik = useFormik({
@@ -12,26 +13,32 @@ const formik = useFormik({
     onSubmit: values => {
         console.log(values);
     },
-    validate: values => {
-        let errors = {}
+    // validate: values => {
+    //     let errors = {}
 
-        if(!values.name) {
-            errors.name = "Required"
-        }
+    //     if(!values.name) {
+    //         errors.name = "Required"
+    //     }
 
-        if(!values.email) {
-            errors.email = "Required"
-        } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)) {
-            errors.email = "Invalid Email Format"
-        }
+    //     if(!values.email) {
+    //         errors.email = "Required"
+    //     } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)) {
+    //         errors.email = "Invalid Email Format"
+    //     }
 
-        if(!values.channel) {
-            errors.channel = "Required"
-        }
+    //     if(!values.channel) {
+    //         errors.channel = "Required"
+    //     }
         
 
-        return errors
-    }
+    //     return errors
+    // }
+    validationSchema: Yup.object({
+      name: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email format").required("Required"),
+      channel: Yup.string().required("Required"),
+    })
+
 })
 console.log(formik.touched)
 
@@ -41,19 +48,19 @@ console.log(formik.touched)
         <div className='controler'>
         <label className="label" htmlFor = "name">Name</label>
         <input type="text" id = "name" name = "name" onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}/>
-        {formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+        {formik.touched.name ? <div className='error'>{formik.errors.name}</div> : null}
         </div>
 
         <div className='controler'>
         <label htmlFor = "email">Email</label>
         <input type="email" id = "email" name = "email" onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur}/>
-        {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+        {formik.touched.email ? <div className='error'>{formik.errors.email}</div> : null}
         </div>
 
         <div className='controler'>
         <label htmlFor = "channel">Channel</label>
         <input type="text" id = "channel" name = "channel" onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur}/>
-        {formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+        {formik.touched.channel ? <div className='error'>{formik.errors.channel}</div> : null}
         </div>
 
         <button type="submit">Submit</button>
